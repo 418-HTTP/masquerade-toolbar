6cf725e26c1ced23644065dc0e67d0f48f97a4b5
Slobodan Prodanovic <sloba.prodanovic@gmail.com>
2025-12-30 14:32:37 +0100

    task - Fixed eslint coding errors.

--- js/masquerade-toolbar.js
+++ js/masquerade-toolbar.js
@@ -4,115 +4,123 @@
  */
 
 (function ($, Drupal, drupalSettings, once) {
-  'use strict';
-
   Drupal.behaviors.masqueradeToolbar = {
-    attach: function (context, settings) {
-      const toolbars = once('masquerade-toolbar', '.masquerade-toolbar', context);
+    attach(context, settings) {
+      const toolbars = once(
+        'masquerade-toolbar',
+        '.masquerade-toolbar',
+        context,
+      );
 
       if (!toolbars.length) {
         return;
       }
 
-      toolbars.forEach(function(toolbar) {
+      toolbars.forEach((toolbar) => {
         const $toolbar = $(toolbar);
         const config = drupalSettings.masqueradeToolbar || {};
         const $toggleBtn = $toolbar.find('.masquerade-toolbar__toggle-btn');
         const $closeBtn = $toolbar.find('.masquerade-toolbar__close');
-        const $searchInput = $toolbar.find('.masquerade-toolbar__search-input');
-        const $autocomplete = $toolbar.find('.masquerade-toolbar__autocomplete');
+        const $searchInput = $toolbar.find(
+          '.masquerade-toolbar__search-input',
+        );
+        const $autocomplete = $toolbar.find(
+          '.masquerade-toolbar__autocomplete',
+        );
 
-      // Toggle toolbar.
-      $toggleBtn.on('click', function () {
-        $toolbar.toggleClass('masquerade-toolbar--expanded');
-      });
-
-      // Close toolbar.
-      $closeBtn.on('click', function () {
-        $toolbar.removeClass('masquerade-toolbar--expanded');
-      });
-
-      // Keyboard shortcut (Alt+M).
-      if (config.keyboardShortcuts) {
-        $(document).on('keydown', function (e) {
-          if (e.altKey && e.key === 'm') {
-            e.preventDefault();
-            $toolbar.toggleClass('masquerade-toolbar--expanded');
-            if ($toolbar.hasClass('masquerade-toolbar--expanded')) {
-              $searchInput.focus();
-            }
-          }
+        // Toggle toolbar.
+        $toggleBtn.on('click', () => {
+          $toolbar.toggleClass('masquerade-toolbar--expanded');
         });
-      }
 
-      // Autocomplete.
-      let autocompleteTimeout;
-      $searchInput.on('input', function () {
-        clearTimeout(autocompleteTimeout);
-        const query = $(this).val();
+        // Close toolbar.
+        $closeBtn.on('click', () => {
+          $toolbar.removeClass('masquerade-toolbar--expanded');
+        });
 
-        if (query.length < 2) {
-          $autocomplete.hide().empty();
-          return;
-        }
-
-        autocompleteTimeout = setTimeout(() => {
-          $.ajax({
-            url: '/masquerade-toolbar/autocomplete',
-            data: { q: query },
-            success: function (data) {
-              $autocomplete.empty();
-
-              if (data.length === 0) {
-                $autocomplete.hide();
-                return;
+        // Keyboard shortcut (Alt+M).
+        if (config.keyboardShortcuts) {
+          $(document).on('keydown', (e) => {
+            if (e.altKey && e.key === 'm') {
+              e.preventDefault();
+              $toolbar.toggleClass('masquerade-toolbar--expanded');
+              if ($toolbar.hasClass('masquerade-toolbar--expanded')) {
+                $searchInput.focus();
               }
-
-              data.forEach(user => {
-                const $item = $('<div class="masquerade-toolbar__autocomplete-item"></div>')
-                  .text(user.label)
-                  .data('uid', user.uid)
-                  .data('url', user.url)
-                  .on('click', function () {
-                    window.location.href = user.url;
-                  });
-                $autocomplete.append($item);
-              });
-
-              $autocomplete.show();
             }
           });
-        }, 300);
-      });
+        }
 
-      // Click outside to close autocomplete.
-      $(document).on('click', function (e) {
-        if (!$(e.target).closest('.masquerade-toolbar__search').length) {
-          $autocomplete.hide();
+        // Autocomplete.
+        let autocompleteTimeout;
+        $searchInput.on('input', function () {
+          clearTimeout(autocompleteTimeout);
+          const query = this.value;
+
+          if (query.length < 2) {
+            $autocomplete.hide().empty();
+            return;
+          }
+
+          autocompleteTimeout = setTimeout(() => {
+            $.ajax({
+              url: '/masquerade-toolbar/autocomplete',
+              data: { q: query },
+              success(data) {
+                $autocomplete.empty();
+
+                if (data.length === 0) {
+                  $autocomplete.hide();
+                  return;
+                }
+
+                data.forEach((user) => {
+                  const $item = $(
+                    '<div class="masquerade-toolbar__autocomplete-item"></div>',
+                  );
+                  $item[0].textContent = user.label;
+                  $item
+                    .data('uid', user.uid)
+                    .data('url', user.url)
+                    .on('click', () => {
+                      window.location.href = user.url;
+                    });
+                  $autocomplete.append($item);
+                });
+
+                $autocomplete.show();
+              },
+            });
+          }, 300);
+        });
+
+        // Click outside to close autocomplete.
+        $(document).on('click', (e) => {
+          if (!$(e.target).closest('.masquerade-toolbar__search').length) {
+            $autocomplete.hide();
+          }
+        });
+
+        // Set initial state.
+        if (config.collapsed) {
+          $toolbar.removeClass('masquerade-toolbar--expanded');
+        } else {
+          $toolbar.addClass('masquerade-toolbar--expanded');
+        }
+
+        // Mobile hide.
+        if (config.mobileHide) {
+          const checkMobile = () => {
+            if (window.innerWidth < 768) {
+              $toolbar.addClass('masquerade-toolbar--mobile-hidden');
+            } else {
+              $toolbar.removeClass('masquerade-toolbar--mobile-hidden');
+            }
+          };
+          checkMobile();
+          $(window).on('resize', checkMobile);
         }
       });
-
-      // Set initial state.
-      if (config.collapsed) {
-        $toolbar.removeClass('masquerade-toolbar--expanded');
-      } else {
-        $toolbar.addClass('masquerade-toolbar--expanded');
-      }
-
-      // Mobile hide.
-      if (config.mobileHide) {
-        const checkMobile = () => {
-          if (window.innerWidth < 768) {
-            $toolbar.addClass('masquerade-toolbar--mobile-hidden');
-          } else {
-            $toolbar.removeClass('masquerade-toolbar--mobile-hidden');
-          }
-        };
-        checkMobile();
-        $(window).on('resize', checkMobile);
-      }
-      });
-    }
+    },
   };
-
 })(jQuery, Drupal, drupalSettings, once);
